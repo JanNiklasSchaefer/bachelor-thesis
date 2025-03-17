8f50a4831d6482f42037789671b572acd1eccdbb
jan_schaefer <jan.schaefer@campus.tu-berlin.de>
2025-03-17 18:29:24 +0100

    deleted unnecessary files

--- benchmarkingLevels/createBenchmarkingSeed.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-# This script was used to generate a random seed to select the benchmarking levels from 1-1000.
-
-random_numbers = random.sample(range(1, 1001), 50)
-random_numbers.sort()
-print(random_numbers)
